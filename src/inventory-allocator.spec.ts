e7155c15b45580e3d743b19c2a97977ce00f1d1c
Jason Law <jasonlaw.dev@gmail.com>
2020-08-24 18:38:48 -0400

    feat: complete allocator logic and add corresponding test cases

--- src/inventory-allocator.spec.ts
+++ src/inventory-allocator.spec.ts
@@ -13,10 +13,69 @@ interface TestCase {
 
 const testCases: TestCase[] = [
   {
-    name: 'Happy case',
+    name: 'simplest case',
     warehouses: [{ name: 'owd', inventory: { apple: 1 } }],
     order: { apple: 1 },
     expectedOutput: [{ owd: { apple: 1 } }],
+  },
+  {
+    name: 'no inventory case 1',
+    warehouses: [{ name: 'owd', inventory: { apple: 0 } }],
+    order: { apple: 1 },
+    expectedOutput: [],
+  },
+  {
+    name: 'no inventory case 2',
+    warehouses: [{ name: 'owd', inventory: { apple: 1 } }, { name: 'dm', inventory: { apple: 3 } }],
+    order: { apple: 5 },
+    expectedOutput: [],
+  },
+  {
+    name: 'no inventory case 3',
+    warehouses: [{ name: 'owd', inventory: { apple: 1, orange: 3 } }, {
+      name: 'dm',
+      inventory: { apple: 4, orange: 1 },
+    }],
+    order: { apple: 5, orange: 5 },
+    expectedOutput: [],
+  },
+  {
+    name: 'invalid order case 1',
+    warehouses: [{ name: 'owd', inventory: { apple: 1 } }],
+    order: { apple: 0 },
+    expectedOutput: [],
+  },
+  {
+    name: 'invalid order case 2',
+    warehouses: [{ name: 'owd', inventory: { apple: 1 } }],
+    order: { apple: 1, orange: -1 },
+    expectedOutput: [],
+  },
+  {
+    name: 'split order case 1',
+    warehouses: [{ name: 'owd', inventory: { apple: 5 } }, { name: 'dm', inventory: { apple: 5 } }],
+    order: { apple: 10 },
+    expectedOutput: [{ owd: { apple: 5 } }, { dm: { apple: 5 } }],
+  },
+  {
+    name: 'split order case 2',
+    warehouses: [{ name: 'owd', inventory: { apple: 7 } }, { name: 'dm', inventory: { apple: 5 } }],
+    order: { apple: 10 },
+    expectedOutput: [{ owd: { apple: 7 } }, { dm: { apple: 3 } }],
+  },
+  {
+    name: 'split order case 3',
+    warehouses: [
+      { name: 'owd', inventory: { apple: 7 } },
+      { name: 'dm', inventory: { apple: 5, pineapple: 7 } },
+      { name: 'jfk', inventory: { pineapple: 10 } },
+    ],
+    order: { apple: 10, pineapple: 15 },
+    expectedOutput: [
+      { owd: { apple: 7 } },
+      { dm: { apple: 3, pineapple: 7 } },
+      { jfk: { pineapple: 8 } },
+    ],
   },
 ];
 
--- src/inventory-allocator.ts
+++ src/inventory-allocator.ts
@@ -1,13 +1,64 @@
 /* eslint-disable no-restricted-syntax */
+import { ItemCounts } from './interfaces/item-counts';
 import { Order } from './interfaces/order';
 import { Shipment } from './interfaces/shipment';
 import { Warehouse } from './interfaces/warehouse';
 
 export class InventoryAllocator {
+  // store all counts in one place
+  private globalWarehouse: Warehouse;
+
   constructor(public warehouses: Warehouse[]) {
+    this.globalWarehouse = {
+      name: 'global',
+      inventory: {},
+    };
+    // add all counts into globalWarehouse
+    warehouses.forEach((warehouse) => {
+      Object.entries(warehouse.inventory).forEach(([name, count]) => {
+        this.globalWarehouse.inventory[name] = (this.globalWarehouse.inventory[name] || 0) + count;
+      });
+    });
   }
 
   process(order: Order): Shipment[] {
-    return [];
+    // make a copy so that we don't modify the original order.
+    const remainingItems: Order = { ...order };
+    const shipments: Shipment[] = [];
+
+    const isValidOrder = Object.values(remainingItems).every((value) => value != null && value > 0);
+    // make sure we have all items before proceeding
+    const hasInventory = Object.entries(remainingItems).every(([name, count]) => (
+      (this.globalWarehouse.inventory[name] || 0) >= count
+    ));
+    if (isValidOrder && hasInventory) {
+      for (const warehouse of this.warehouses) {
+        const shipmentItems: ItemCounts = {};
+        Object.entries(remainingItems).forEach(([name, count]) => {
+          if (warehouse.inventory[name] != null && warehouse.inventory[name] > 0) {
+            const numbersToShip = Math.min(warehouse.inventory[name], count);
+
+            shipmentItems[name] = numbersToShip;
+
+            warehouse.inventory[name] -= numbersToShip;
+            this.globalWarehouse.inventory[name] -= numbersToShip;
+            remainingItems[name] -= numbersToShip;
+
+            // delete fulfilled item to speed up
+            if (remainingItems[name] === 0) {
+              delete remainingItems[name];
+            }
+          }
+        });
+        if (Object.keys(shipmentItems).length > 0) {
+          shipments.push({ [warehouse.name]: shipmentItems });
+        }
+        // terminate early if all items are fulfilled
+        if (Object.keys(remainingItems).length === 0) {
+          break;
+        }
+      }
+    }
+    return shipments;
   }
 }
